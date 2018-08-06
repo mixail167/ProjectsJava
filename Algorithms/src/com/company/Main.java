e5d7c76d702bffc36e33e7b74e12f963ed0e694b
mixail167 <mixailkovalev167@mail.ru>
2018-08-06 16:33:55 +0300

    Алгоритмы поиска в массиве

--- Algorithms/src/com/company/Main.java
+++ Algorithms/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -10,15 +11,55 @@ public class Main {
         return (row * row + row) / 2 + column;
     }
 
+    private static int InterpolationSearch(int[] sortedArray, int toFind) {
+        int mid;
+        int low = 0;
+        int high = sortedArray.length - 1;
+        while (sortedArray[low] < toFind && sortedArray[high] > toFind) {
+            mid = low + ((toFind - sortedArray[low]) * (high - low)) / (sortedArray[high] - sortedArray[low]);
+
+            if (sortedArray[mid] < toFind)
+                low = mid + 1;
+            else if (sortedArray[mid] > toFind)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        if (sortedArray[low] == toFind)
+            return low;
+        else if (sortedArray[high] == toFind)
+            return high;
+        else
+            return -1;
+    }
+
+    private static int BinarySearch(int[] sortedArray, int toFind) {
+        int mid;
+        int low = 0;
+        int high = sortedArray.length - 1;
+        while (low <= high) {
+            mid = (int) ((low + high) * 1.0 / 2);
+            if (sortedArray[mid] < toFind)
+                low = mid + 1;
+            else if (sortedArray[mid] > toFind)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+
     public static void main(String[] args) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Треугольный массив");
         int n = 0;
+        int[] array = new int[1];
         do {
             System.out.print("Введите N:");
             try {
                 n = Integer.parseInt(bufferedReader.readLine());
-                int[] array = new int[(n * n + n) / 2];
+                array = new int[(n * n + n) / 2];
                 Random r = new Random();
                 for (int i = 0; i < n; i++)
                     for (int j = 0; j < n && i >= j; j++)
@@ -32,6 +73,32 @@ public class Main {
 
             }
         } while (n <= 0);
+        System.out.println("Интерполяционный поиск в треугольном массиве");
+        while (true) {
+            try {
+                System.out.print("Введите целевой элемент:");
+                n = Integer.parseInt(bufferedReader.readLine());
+                Arrays.sort(array);
+                int index = InterpolationSearch(array, n);
+                System.out.printf("Результат: %d\n", index);
+                break;
+            } catch (Exception ignored) {
+
+            }
+        }
+        System.out.println("Бинарный поиск в треугольном массиве");
+        while (true) {
+            try {
+                System.out.print("Введите целевой элемент:");
+                n = Integer.parseInt(bufferedReader.readLine());
+                Arrays.sort(array);
+                int index = BinarySearch(array, n);
+                System.out.printf("Результат: %d\n", index);
+                break;
+            } catch (Exception ignored) {
+
+            }
+        }
         System.out.println("Массив с разрывом");
         ArrayRow arrayRow = new ArrayRow();
         n = 10;
